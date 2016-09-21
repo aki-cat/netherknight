47d7a85559058d810c70c114e0816b993bfcef7a
orenjiakira <orenjiakira@gmail.com>
2016-09-21 20:00:44 -0300

    Add blinglevel to gamedata

--- src/gamedata.lua
+++ src/gamedata.lua
@@ -5,6 +5,7 @@ return {
   level     = 0,
   killcount = 0,
   damage    = 0,
+  blinglvl  = 1,
   inventory = {},
   weapon    = { name = "sord", __type = "sword" },
   map       = { { id = 0, enemies = {}, items = {} } },
