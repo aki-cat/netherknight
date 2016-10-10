14f24779e94809208eede3e6921e0fed22f7e03d
orenjiakira <orenjiakira@gmail.com>
2016-10-09 23:48:30 -0300

    Fix create release script

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm netherknight.love
-zip -r9 netherknight.love * -x test*
+# NOTE: only run this when your bash/shell section is on the project's root folder
+
+cd "$(pwd)"/src
+
+rm ../netherknight.love
+zip -r9 ../netherknight.love * -x test*
+
+cd ..
 
