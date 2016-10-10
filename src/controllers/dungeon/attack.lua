baa0354e2aa04f8f4a586241db1ac46f05bb52b5
orenjiakira <orenjiakira@gmail.com>
2016-10-10 19:45:36 -0300

    Change attack distance?

--- src/controllers/dungeon/attack.lua
+++ src/controllers/dungeon/attack.lua
@@ -45,8 +45,8 @@ function attack:__init ()
       basic_slash_sprite:set_flip_h(true)
     end
 
-    basic_slash_hitbox:set_pos(pos + dir * 1)
-    basic_slash_sprite:set_pos((pos + dir * 1):unpack())
+    basic_slash_hitbox:set_pos(pos + dir * 1.5)
+    basic_slash_sprite:set_pos((pos + dir * 1.5):unpack())
 
     attack_model:add_element(basic_slash_attack)
     hitbox_model:add_element(basic_slash_hitbox)
